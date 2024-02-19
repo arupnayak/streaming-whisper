1b45adf6a27a5fe01271efef3c5aa62c3b680d3e
Arup Nayak <arup.nayak@gmail.com>
2024-02-19 15:10:36 +0400

    Add audio file upload and transcription endpoint

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from faster_whisper import WhisperModel, decode_audio
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, File, UploadFile
 from starlette.responses import StreamingResponse
 import time
+import asyncio
 
 app = FastAPI()
 
 model_size = "base"
-model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
 # Run on GPU with FP16
 # model = WhisperModel(model_size, device="cuda", compute_type="float32")
@@ -15,25 +15,50 @@ model = WhisperModel(model_size, device="cpu", compute_type="int8")
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
 async def transcript(
         audio_file_name: str
 ):
-    start_time = time.time()
-    segments, info = model.transcribe(audio_file_name, beam_size=5)
-    print("Transcription took %.2fs" % (time.time() - start_time))
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-    yield_time = time.time()
-    for segment in segments:
-        print("Yield took %.2fs" % (time.time() - yield_time))
-        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-        yield segment.text
+    try:
+        start_time = time.time()
+        print("Transcribing %s" % audio_file_name)
+        segments, info = model.transcribe(audio_file_name, beam_size=5)
+        print("Transcription took %.2fs" % (time.time() - start_time))
+        print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+        for segment in segments:
+            print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+            yield segment.text
+        # delete audio file
+        os.remove(audio_file_name)
+    except asyncio.CancelledError:
+        print("Cancelled")
+        return
 
-@app.get("/transcript")
-async def transcript_endpoint(audio_file_name: str = Query()):
-    # set header to stream the response
-    return StreamingResponse( transcript(audio_file_name))
+@app.post("/transcript")
+async def transcript_endpoint(audio_file_name: str = Query(),
+                              audio_file: UploadFile = File(...)):
+    if audio_file:
+        audio_file_name = time.strftime("%Y%m%d-%H%M%S") + audio_file.filename
+        # save audio_file to disk
+        with open(audio_file_name, "wb") as buffer:
+            buffer.write(audio_file.file.read())
+    return StreamingResponse(transcript(audio_file_name))
 
+async def generator():
+    try:
+        for i in range(100):
+            yield "Hello World"
+            print("Hello World")
+            await asyncio.sleep(1)
+    except asyncio.CancelledError:
+        print("Cancelled")
+        return
+
+@app.post("/")
+async def main(file: UploadFile = File(...)):
+    return StreamingResponse(generator())
+    
 def test_stereo_diarization(data_dir):
     model = WhisperModel("tiny")
 
